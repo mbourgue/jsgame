db5429b82df0416bcd8906683702588d9e51dc20
Morpheus <bourguematthieu@gmail.com>
2016-02-24 21:25:21 +0100

    basic class creation

--- js/grid.js
+++ js/grid.js
@@ -1,24 +1,50 @@
+var key_value =
+[
+    'Q',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'M',
+];
+
+
+// Grid
 function Grid() {
 
   var tracks;
 
-  this.update = function () {
+  this.update = function () { // Update all tracks
+    tracks.update();
+  }
+  this.pause = function() {
 
   }
 
 }
 
 
-
-
+// Track Mgr
 function Tracks() {
 
+  var tracks = [];
 
-  this.add = function (lines) {
+
+  this.add = function (lines) { // Add one track
 
   }
-  this.delete = function (lines) {
+  this.delete = function (lines) { // Delete one track
 
+  }
+
+  this.update = function () {
+    for(track in tracks) {
+      track.update();
+    }
   }
   // this. = function () {
   //
@@ -29,9 +55,47 @@ function Tracks() {
 
 // KEY ID: QSDFGHJKLM
 
+// Track
 function Track() {
 
-  var key_id;
+  var keyboard_id;
+  var Key;
+
+  var notes = []; // tableau de notes
 
 
+  var line = new Phaser.Rectangle(50, 0, 5, 500);
+  var color = "#FFF";
+
+
+  this.update = function() {
+
+    for(note in notes) {
+      note.update();
+    }
+
+  }
+}
+
+
+
+
+
+
+// Note
+function Note() {
+
+  this.update = function() {
+
+  }
+
+}
+
+// Key
+function Key(value) {
+  var value = "A";
+
+  this.push = function() {
+
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,30 +1,61 @@
 
 
-var game = new Phaser.Game(window.pageX, window.pageY, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(window.pageX, window.pageY, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render  });
+
+
+
+var grid = new Grid();
+
+
 
 function preload() {
 
-  /*  game.load.spritesheet('button', 'assets/buttons/button_sprite_sheet.png', 193, 71);
-    game.load.image('background','assets/misc/starfield.jpg');*/
+    game.load.spritesheet('button', 'assets/buttons/button_sprite_sheet.png', 193, 71);
+    game.load.image('background','assets/misc/starfield.jpg');
 
 }
 
 var button;
 var background;
 
+
+var line;
+
 function create() {
 
-    /*game.stage.backgroundColor = '#182d3b';
+    game.stage.backgroundColor = '#222';
 
-    background = game.add.tileSprite(0, 0, 800, 600, 'background');
+    // background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
     button = game.add.button(game.world.centerX - 95, 400, 'button', actionOnClick, this, 2, 1, 0);
 
-    button.onInputOver.add(over, this);
+  /*  button.onInputOver.add(over, this);
     button.onInputOut.add(out, this);
     button.onInputUp.add(up, this);*/
 
+    line = new Phaser.Rectangle(50, 0, 5, 500);
+
+
 }
+
+
+
+
+function update() {
+
+    /*line.centerOn(game.input.activePointer.x, game.input.activePointer.y);
+    line.rotate(0.05);*/
+
+}
+
+function render() {
+
+    game.debug.geom(line, '#FFF');
+    /*game.debug.lineInfo(line, 32, 32);*/
+
+}
+
+
 
 function up() {
     console.log('button up', arguments);
